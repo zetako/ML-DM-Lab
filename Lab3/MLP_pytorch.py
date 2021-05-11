883232497be340333eebe3e4c37db60d7a9cbd25
zetako <zetako@outlook.com>
2021-05-11 10:32:44 +0800

    MLP Done

--- Lab3/MLP_pytorch.py
+++ Lab3/MLP_pytorch.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as func
 import numpy as np
 
 # Hyperparameter
@@ -6,6 +7,20 @@ NUM_EPOCHS = 5000
 DEVICE = 'cuda:0'
 USE_CUDA = False
 DISPLAY = 1000
+
+# MLP class
+class MLP(torch.nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.l1 = torch.nn.Linear(32*32*3, 16*16*3)
+        self.l2 = torch.nn.Linear(8*8*3, 4*4*3)
+        self.l3 = torch.nn.Linear(4*4*3, 10)
+
+    def forward(self, x):
+        x = func.relu(l1(x))
+        x = func.relu(l2(x))
+        x = func.softmax(l3(x), dim = 1)
+        return x
 
 # Dataset
 train_images = np.load("cifar10_train_images.npy")
@@ -17,9 +32,9 @@ test_size = test_images.shape[0]
 
 
 # Linear Model
-linear_model = torch.nn.Linear(32*32*3, 10)
+MLP_model = MLP()
 loss_func = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(linear_model.parameters())
+optimizer = torch.optim.Adam(MLP.parameters())
 
 # Dataset to Pytorch
 train_images = torch.from_numpy(train_images.astype(np.float32))
@@ -27,7 +42,7 @@ train_labels = torch.from_numpy(train_labels)
 
 # Training
 if USE_CUDA:
-    linear_model = linear_model.to(DEVICE)
+    MLP_model = MLP_model.to(DEVICE)
 
 for epoch in range(NUM_EPOCHS):
     # data this time
@@ -41,7 +56,7 @@ for epoch in range(NUM_EPOCHS):
 
     # cal
     optimizer.zero_grad()
-    pred = linear_model(x)
+    pred = MLP_model(x)
     loss = loss_func(pred, label)
     loss.backward()
     optimizer.step()
